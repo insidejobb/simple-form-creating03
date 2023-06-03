e579e381b0beded4ed338abd76621c7316e90081
insidejobb <bhavanii1924@gmail.com>
2023-06-03 01:31:33 -1200

    new Project started

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const storedUserLoggedInInformation=localStorage.getItem('isLoggedIn')
+  useEffect(()=>{
+   
+    const storedUserLoggedInInformation=localStorage.getItem('isLoggedIn')
+  
+    if(storedUserLoggedInInformation===1){
+      setIsLoggedIn(true);
+    }
 
-  if(storedUserLoggedInInformation===1){
-    setIsLoggedIn(true);
-  }
-  useEffect
+  },[]);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -30,4 +31,13 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLo
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
